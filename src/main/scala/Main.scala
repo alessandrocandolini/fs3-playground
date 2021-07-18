853be1d0ef04b1b6ebb236565333337ec161cf4d
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-07-18 10:13:43 +0100

    Some streams and sbt 1.5.5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,13 +9,18 @@ import Erratum._
 import scala.CanEqual.derived
 import org.w3c.dom.NamedNodeMap
 import cats.data.EitherT
+import fs2._
 
 object Main extends IOApp.Simple:
-  val run: IO[Unit] = ioProgram
+  val run: IO[Unit] = program
 
 enum Erratum derives CanEqual:
   case NaN
   case NegativeNumber
+
+def program: IO[Unit] = ioProgram
+
+val f: Stream[IO, String] = Stream.eval(IO.readLine)
 
 def ioProgram: IO[Unit] = input
   .map(pureProgram)
@@ -29,15 +34,13 @@ val input: EitherT[IO, Erratum, Int] = EitherT(
     .map(x => parse(x).flatMap(validate))
 )
 
-val display: Either[Erratum, List[String]] => String = _.fold(displayErratum, _.mkString("\n"))
-
-val displayErratum: Erratum => String = _.toString
+val display: Either[Erratum, List[String]] => String = _.fold(_.toString, _.mkString("\n"))
 
 val parse: String => Either[Erratum, Int] = _.toIntOption.toRight[Erratum](NaN)
 
 val validate: Int => Either[Erratum, Int] = x => Either.cond(x > 0, x, NegativeNumber)
 
-val pureProgram: Int => List[String] = n => List.range(0, n).map(fizzbuzz)
+val pureProgram: Int => List[String] = n => List.range(0, n).map(fizzbuzzAndRender)
 
 enum FizzBuzz derives CanEqual:
   case FizzBuzz
@@ -45,19 +48,19 @@ enum FizzBuzz derives CanEqual:
   case Buzz
   case Other(n: Int)
 
-val fizzbuzzImproved: Int => FizzBuzz =
+val fizzbuzz: Int => FizzBuzz        =
   case n if n % 3 == 0 && n % 5 == 0 => FizzBuzz.FizzBuzz
   case n if n % 3 == 0 => Fizz
   case n if n % 5 == 0 => Buzz
   case n => Other(n)
 
-val render: FizzBuzz => String        =
+val render: FizzBuzz => String       =
   case FizzBuzz.FizzBuzz => "FizzBuzz"
   case Fizz              => "Fizz"
   case Buzz              => "Buzz"
   case Other(n)          => s"$n"
 
-val fizzbuzz: Int => String           = render compose fizzbuzzImproved
+val fizzbuzzAndRender: Int => String = render compose fizzbuzz
 
 val fizzbuzzLegacy: Int => String =
   case n if n % 3 == 0 && n % 5 == 0 => "FizzBuzz"
