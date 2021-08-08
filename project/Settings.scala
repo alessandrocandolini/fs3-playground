04e94e1f6379e5e085ad558cdebc50f82521eab1
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-08-08 16:19:14 +0100

    Bump dependencies

--- project/Settings.scala
+++ project/Settings.scala
@@ -24,12 +24,16 @@ object Dependencies {
     "org.typelevel" %% "cats-effect" % Versions.catsEffect
   )
 
-  val fs2          = Seq(
+  val fs2   = Seq(
     "co.fs2" %% "fs2-core",
     "co.fs2" %% "fs2-io"
   ).map(_ % Versions.fs2)
 
-  val dependencies = cats ++ fs2
+  val tapir = Seq(
+    "com.softwaremill.sttp.tapir" %% "tapir-core" % Versions.tapir
+  )
+
+  val dependencies = cats ++ fs2 ++ tapir
 
   val testDependencies = Seq(
     "org.scalacheck" %% "scalacheck"          % Versions.scalacheck,
@@ -42,10 +46,11 @@ object Versions {
 
   val scala                   = "3.0.1"
   val catsCore                = "2.6.1"
-  val catsEffect              = "3.1.1"
+  val catsEffect              = "3.2.2"
   val munitCatsEffect         = "1.0.3"
   val scalacheckEffectVersion = "1.0.2"
-  val fs2                     = "3.0.6"
+  val fs2                     = "3.1.0"
+  val tapir                   = "0.19.0-M4"
   val scalacheck              = "1.15.4"
 
 }
