2a14fe23208850b2069e3cebf1d8ac589bc3db45
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-09-30 21:31:31 +0100

    upgrades

--- build.sbt
+++ build.sbt
@@ -4,17 +4,14 @@ import Settings._
 lazy val root = project
   .in(file("."))
   .settings(commonSettings)
+  .settings(Defaults.itSettings)
   .settings(
-    name := "fs3-playground",
-    version := "0.1.0",
-    scalaVersion := Versions.scala,
+    name                       := "fs3-playground",
+    version                    := "0.1.0",
+    scalaVersion               := Versions.scala,
     assembly / assemblyJarName := "fs3-playground.jar",
-    assembly / test := Def
-      .sequential(
-        Test / test,
-        IntegrationTest / test
-      )
-      .value,
-    scalafmtOnCompile := true,
+    assembly / test            := Def.sequential(Test / test, IntegrationTest / test).value,
+    scalafmtOnCompile          := false,
     libraryDependencies ++= dependencies ++ testDependencies
   )
+  .configs(IntegrationTest)
---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,62 @@
+import cats.effect.IO
+import cats.instances.list
+import cats.syntax.traverse
+import cats.Traverse.ops.toAllTraverseOps
+import cats.effect.IOApp
+import cats.effect.ExitCode
+import FizzBuzz._
+import Erratum._
+import scala.CanEqual.derived
+import org.w3c.dom.NamedNodeMap
+import cats.data.EitherT
+import fs2._
+
+enum Erratum derives CanEqual:
+  case NaN
+  case NegativeNumber
+
+enum FizzBuzz derives CanEqual:
+  case FizzBuzz
+  case Fizz
+  case Buzz
+  case Other(n: Int)
+
+def ioProgram: IO[Unit] = input
+  .map(pureProgram)
+  .value
+  .map(display)
+  .flatMap(IO.println)
+
+val pureProgram: Int => List[String] = n => List.range(0, n).map(fizzbuzzAndRender)
+
+val input: EitherT[IO, Erratum, Int] = EitherT(
+  IO.println("insert a positive number:")
+    .flatMap(_ => IO.readLine)
+    .map(x => parse(x).flatMap(validate))
+)
+
+val display: Either[Erratum, List[String]] => String = _.fold(_.toString, _.mkString("\n"))
+
+val parse: String => Either[Erratum, Int] = _.toIntOption.toRight[Erratum](NaN)
+
+val validate: Int => Either[Erratum, Int] = x => Either.cond(x > 0, x, NegativeNumber)
+
+val fizzbuzz: Int => FizzBuzz        =
+  case n if n % 3 == 0 && n % 5 == 0 => FizzBuzz.FizzBuzz
+  case n if n % 3 == 0 => Fizz
+  case n if n % 5 == 0 => Buzz
+  case n => Other(n)
+
+val render: FizzBuzz => String       =
+  case FizzBuzz.FizzBuzz => "FizzBuzz"
+  case Fizz              => "Fizz"
+  case Buzz              => "Buzz"
+  case Other(n)          => s"$n"
+
+val fizzbuzzAndRender: Int => String = render compose fizzbuzz
+
+val fizzbuzzLegacy: Int => String =
+  case n if n % 3 == 0 && n % 5 == 0 => "FizzBuzz"
+  case n if n % 3 == 0 => "Fizz"
+  case n if n % 5 == 0 => "Buzz"
+  case n => s"$n"
